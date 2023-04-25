60b4c99693d08256e1cec74c1c6f533e6db8716b
anasimas <ana.simas2109@outlook.com>
2023-04-25 16:03:58 -0300

    new: finalizado o layout, iniciado o JS

---
+++ index.js
@@ -0,0 +1,5 @@
+const opcoesLinguagens = ['JavaScript', 'PHP', 'Python', 'TypeScript'];
+const APIKey = document.getElementById('APIkey');
+const botaoTraduzir = document.getElementById('botaoTraduzir');
+const LinguagemInput = document.getElementById('ling_input').value;
+const LinguagemOutput = document.getElementById('ling_output').value;
